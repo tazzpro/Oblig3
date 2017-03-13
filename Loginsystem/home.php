9e211872a864ee94b59e4ec875c64be348c307bc
stryaxx <espensv@stud.ntnu.no>
2017-03-13 14:32:02 +0100

    Endringer med bilder
    Her er nye endringer med bilder som Bjørn-Magnus har laget

--- Loginsystem/home.php
+++ Loginsystem/home.php
@@ -9,10 +9,23 @@ $db=mysqli_connect("localhost","root","Asdasd123","authentication");
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
+	<body class="background">
 	<div class="header">
-		<h1>Register, login and logout user php mysql</h1>
+		<h1>Welcome to Mega Forum</h1>
 	</div>
-	<h1>Home</h1>
+	
+		<header>
+			<nav>
+				<tr>
+				<td><a href="#">Home</a></td>
+				<td><a href="#">Forum</a></td>
+				<td><a href="#">About Us</a></td>
+				<td><a href="#">Contact Us</a></td>
+				</tr>
+			</nav>
+	</header>
+	<img src="start1.jpg" style="float: right; height: 15em; margin-right: 5%; margin-bottom: 0.5em;">
+	
 	<div><h4>Welcome <?php echo $_SESSION['username'];	?></h4></div>
 	
 	<a href="register.php" alt="click to register">Registration</a>
