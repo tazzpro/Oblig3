04b6dc1d58f14332e6f8668f585c39166762fceb
evenmyhre <evenmyh@stud.ntnu.no>
2017-03-15 13:15:51 +0100

    La inn endringene jeg gjorde i \index.html i \home.php
    nice

--- Loginsystem/home.php
+++ Loginsystem/home.php
@@ -1,36 +1,60 @@
-<?php	session_start();
-$db=mysqli_connect("localhost","root","Asdasd123","authentication");
+<?php
+session_start();
+$db = mysqli_connect("localhost", "root", "Asdasd123", "authentication");
 ?>
 
 <!DOCTYPE html>
 <html>
-<head>
-	<title> Register </title>
-	<link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-	<body class="background">
-	<div class="header">
-	<img src="Header.png">
-	</div>
-	<br>
-		<header>
-			<nav>
-				<tr>
-				<td><a href="home.php">Home</a></td>
-				<td><a href="forum.php">Forum</a></td>
-				<td><a href="about.php">About Us</a></td>
-				<td><a href="contact.php">Contact Us</a></td>
-				</tr>
-			</nav>
-	</header>
-	<br>
-	<img src="start1.jpg" style="float: right; height: 15em; margin-right: 5%; margin-bottom: 0.5em;">
-	<h1>Home</h1>
-	<div><h4>Welcome <?php echo $_SESSION['username'];	?></h4></div>
-	
-	<a href="register.php" alt="click to register" id="register">Registration</a>
-	<a href="logout.php" alt="click to sign out" id="logout">Logout</a>
+    <head>
+        <title> Register </title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+    <body class="background">
+        <div class="header">
+            <img src="Header.png">
+        </div>
+        <br>
+        <header>
+            <nav>
+                <tr>
+                    <td><a href="home.php">Home</a></td>
+                    <td><a href="forum.php">Forum</a></td>
+                    <td><a href="about.php">About Us</a></td>
+                    <td><a href="contact.php">Contact Us</a></td>
+                </tr>
+            </nav>
+        </header>
+        
+        <main>
+            <h2>Welcome to MegaForum!</h2>
 
-</body>
+            <div class="content">
+                <h3>    This is a forum where you can discuss just about anything.
+                    <br>It is divided into sub-forums.
+                    <br>Some forums might not be safe for work, so take precautions.
+                    <br>GL HF.
+                </h3>
+            </div>
+
+            <aside>Recent activity:
+                <br>Most recent post edited
+                <br>2nd most recent post edited
+                <br>3rd etc..
+            </aside>
+
+
+        </main>
+        
+        <footer>Created by MegaForum © Copyright! All rights reserved ® 13.03.2017 </footer>
+        
+        
+        <img src="start1.jpg" style="float: right; height: 15em; margin-right: 5%; margin-bottom: 0.5em;">
+        <h1>Home</h1>
+        <div><h4>Welcome <?php echo $_SESSION['username']; ?></h4></div>
+
+        <a href="register.php" alt="click to register" id="register">Registration</a>
+        <a href="logout.php" alt="click to sign out" id="logout">Logout</a>
+
+    </body>
 </html>
