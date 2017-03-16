cef7f47ca670b6ba0e0dc0d40e8f31c5aaf865f8
Espen Vad <espensv@stud.ntnu.no>
2017-03-16 18:16:35 +0100

    contact og style
    Lasta ned fra serveren

--- Loginsystem/contact.php
+++ Loginsystem/contact.php
@@ -21,24 +21,31 @@
             </nav>
         </header>
         <br>
-        <div class="contactinfo">
-            <h1>We're never far away</h1> 
-        <aside class="right">
+         <aside class="right">
         <a href="register.php" alt="click to register" id="register">Registration</a>
 	<a href="logout.php" alt="click to sign out" id="logout">Logout</a>
 	<img src="start1.jpg" id ="rightpic">
         </aside>
+                <aside class="left">Recent activity:
+            <br>Most recent post edited
+            <br>2nd most recent post edited
+            <br>3rd etc..
+        </aside>
+        <div class="content">
+            <h1>We're never far away</h1> 
+       
             <h3><strong><span8>Chat live with customer service</span></strong></h3>
             <p>We're open from 08:00-20:00 monday-friday<br>saturday and sunday from 10:00-16:00</p>
 
             <h3><strong>Call us</strong></h3>
-            <p>We're open monday-friday 08:00 - 20:00<br>
-                saturday and sunday from 10:00-16:00<br>
-                78 34 65 27</p>
+            <p>We're open on: Monday-Friday, 08:00 - 20:00<br>
+                Saturday and Sunday from: 10:00-16:00<br>
+                You can call us on: 78 34 65 27</p>
 
-            <h3><strong>Send an e-mail</strong></h3>
+            <h3><strong>Send an e-mail to MegaForum</strong></h3>
             <p>Send an email to:<br><a href="mailto:customerservice@megaforum.com">customerservice@megaforum.com</a></p>
         </div>
-        <footer><p>Created by MegaForum © Copyright! All rights reserved ® 13.03.2017 </p></footer>
+        <br>
+        <footer><p>Created by MegaForum &copy; Copyright! All rights reserved &reg; 16.03.2017 </p></footer>
     </body>
 </html>
