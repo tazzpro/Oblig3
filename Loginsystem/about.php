a21b100c10dfc438606056ab55955c4b7316e7b3
Espen Vad <espensv@stud.ntnu.no>
2017-03-16 19:41:46 +0100

    About

--- Loginsystem/about.php
+++ Loginsystem/about.php
@@ -22,12 +22,29 @@
 	</header>
 	<br>
         <aside class="right">
-        <a href="register.php" alt="click to register" id="register">Registration</a>
-	<a href="logout.php" alt="click to sign out" id="logout">Logout</a>
-	<img src="start1.jpg" id ="rightpic">
+			<a href="register.php" alt="click to register" id="register">Registration</a>
+			<a href="logout.php" alt="click to sign out" id="logout">Logout</a>
+			<img src="start1.jpg" id ="rightpic">
         </aside>
-	<h1>About us</h1>
-	<div><h4>Welcome <?php echo $_SESSION['username'];	?></h4></div>
+		<aside class="left">Recent activity:
+            <br>Most recent post edited
+            <br>2nd most recent post edited
+            <br>3rd etc..
+        </aside>
+		
+		<div class="content">
+            <h1>About us</h1> 
+       
+            <h3><strong><span8>The developers...</span></h3>
+            <p>We're a group of 6 students
+			<br>that chosen this as a group project.
+			<br>The developers behind this forum are:
+			<br>Rasmus D, Espen V, Eivind M,
+			<br>Bjørn-Magnus S, Knut Johan S and Even M
+			</p></strong>
+
+
+		</div>
 	
 
 </body>
