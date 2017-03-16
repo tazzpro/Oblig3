7b14d5e336888c53126fb1b530e365592e2da6c2
kajah90 <kjselnes@gmail.com>
2017-03-16 12:28:03 +0100

    left/right class, og content class
    laga til left/right class, og content, og la det til i home.

--- Loginsystem/home.php
+++ Loginsystem/home.php
@@ -24,42 +24,48 @@ $db = mysqli_connect("localhost", "root", "Asdasd123", "authentication");
 				</tr>
 			</nav>
 	        </header>
-	<br>
         <aside class="right">
-        <a href="register.php" alt="click to register" id="register">Registration</a>
-	<a href="logout.php" alt="click to sign out" id="logout">Logout</a>
-	<img src="start1.jpg" id ="rightpic">
+            <a href="register.php" alt="click to register" id="register">Registration</a>
+            <a href="logout.php" alt="click to sign out" id="logout">Logout</a>
+            <img src="start1.jpg" id ="rightpic">
         </aside>
-	<h1>Home</h1>
-	<div><h4>Welcome <?php echo $_SESSION['username'];	?></h4></div>
-
-            <div class="content">
-                <h3>    This is a forum where you can discuss just about anything.
-                    <br>It is divided into sub-forums.
-                    <br>Some forums might not be safe for work, so take precautions.
-                    <br>GL HF.
-                </h3>
-            </div>
-
-            <aside>Recent activity:
-                <br>Most recent post edited
-                <br>2nd most recent post edited
-                <br>3rd etc..
-            </aside>
-
-
-        </main>
-        
-        <footer>Created by MegaForum © Copyright! All rights reserved ® 13.03.2017 </footer>
-        
-        
-        <aside class="right">
-        <a href="register.php" alt="click to register" id="register">Registration</a>
-	<a href="logout.php" alt="click to sign out" id="logout">Logout</a>
-	<img src="start1.jpg" id ="rightpic">
+                <aside class="left">Recent activity:
+            <br>Most recent post edited
+            <br>2nd most recent post edited
+            <br>3rd etc..
         </aside>
         <h1>Home</h1>
-        <div><h4>Welcome <?php echo $_SESSION['username']; ?></h4></div>
+        <div class="content">
+            <div><h4>Welcome <?php echo $_SESSION['username'];	?></h4></div>
+            <h3>    This is a forum where you can discuss just about anything.
+                <br>It is divided into sub-forums.
+                <br>Some forums might not be safe for work, so take precautions.
+                <br>GL HF.
+            </h3>
+        </div>
+        <footer><p>Created by MegaForum © Copyright! All rights reserved ® 13.03.2017 </p></footer>
+        
+        
+                <aside class="right">
+            <a href="register.php" alt="click to register" id="register">Registration</a>
+            <a href="logout.php" alt="click to sign out" id="logout">Logout</a>
+            <img src="start1.jpg" id ="rightpic">
+        </aside>
+                <aside class="left">Recent activity:
+            <br>Most recent post edited
+            <br>2nd most recent post edited
+            <br>3rd etc..
+        </aside>
+        <h1>Home</h1>
+        <div class="content">
+            <div><h4>Welcome <?php echo $_SESSION['username'];	?></h4></div>
+            <h3>    This is a forum where you can discuss just about anything.
+                <br>It is divided into sub-forums.
+                <br>Some forums might not be safe for work, so take precautions.
+                <br>GL HF.
+            </h3>
+        </div>
+        <footer><p>Created by MegaForum © Copyright! All rights reserved ® 13.03.2017 </p></footer>
 
     </body>
 </html>
