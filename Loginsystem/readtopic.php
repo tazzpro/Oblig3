1337477496402c5dbbccc5bc36ce7006fdc1ba61
kajah90 <kjselnes@gmail.com>
2017-03-17 16:04:50 +0100

    flytta login button og la til header (readtopic)

--- Loginsystem/readtopic.php
+++ Loginsystem/readtopic.php
@@ -1,57 +1,67 @@
 <?php
-	include ('layout_manager.php');
-	include ('content_function.php');
-	addview($_GET['cid'], $_GET['scid'], $_GET['tid']);
+include ('layout_manager.php');
+include ('content_function.php');
+addview($_GET['cid'], $_GET['scid'], $_GET['tid']);
 ?>
 
 <html>
 
-<head>
-<title> Hello </title>
-<link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-<a href="login.php" alt="Click to log in!">Login</a><br>
-<a href="register.php" alt="Click to log in!">or Register</a>
+    <head>
+        <title> Hello </title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <div class="header">
+            <img src="Header.png">
+        </div>
+        <br>
+        <header>
+            <nav>
+                <tr>
+                    <td><a href="home.php">Home</a></td>
+                    <td><a href="forum.php">Forum</a></td>
+                    <td><a href="about.php">About Us</a></td>
+                    <td><a href="contact.php">Contact Us</a></td>
+                </tr>
+            </nav>
+        </header>
+        <br>
+        <div class="right">
 
-	<div>
-	
-		<?php
-			session_start();
-			if (isset($_SESSION['username'])) {
-				logout();
-			}else {
-				if (isset($_GET['status']))
-				{
-					if($_GET['status'] == 'reg_success') {
-						echo "<h1 style='color:green;'> new user registered successfully!</h1>";
-					}else if($_GET['status'] == 'login_fail') {
-						echo "<h1 style='color: red;'>Invalid username and/or password!</h1>";
-					}				
-				}
-				loginform();
-			}
-		?> 
-		
-	</div>
-	<div class="forumdesc">
-		<?php
-		
-			replylink($_GET['cid'], $_GET['scid'], $_GET['tid']);
-			
-		?>
-	</div>
-	
-	<?php
-		disptopic($_GET['cid'], $_GET['scid'], $_GET['tid']);
-		echo "<div class='content'><p>All Replies(";
-		countReplies($_GET['cid'], $_GET['scid'], $_GET['tid']);
-		echo ")	</p></div>";
-		dispreplies($_GET['cid'], $_GET['scid'], $_GET['tid']);
-	?>
+            <?php
+            session_start();
+            if (isset($_SESSION['username'])) {
+                logout();
+            } else {
+                if (isset($_GET['status'])) {
+                    if ($_GET['status'] == 'reg_success') {
+                        echo "<h1 style='color:green;'> new user registered successfully!</h1>";
+                    } else if ($_GET['status'] == 'login_fail') {
+                        echo "<h1 style='color: red;'>Invalid username and/or password!</h1>";
+                    }
+                }
+                loginform();
+            }
+            ?> 
 
-	
-</body>
+            <img src="start1.jpg" id ="rightpic">
+        </div>
+        <div class="forumdesc">
+            <?php
+            replylink($_GET['cid'], $_GET['scid'], $_GET['tid']);
+            ?>
+        </div>
+
+        <?php
+        disptopic($_GET['cid'], $_GET['scid'], $_GET['tid']);
+        echo "<div class='content'><p>All Replies(";
+        countReplies($_GET['cid'], $_GET['scid'], $_GET['tid']);
+        echo ")	</p></div>";
+        dispreplies($_GET['cid'], $_GET['scid'], $_GET['tid']);
+        ?>
+
+
+    </body>
 
 
 </html>
